53340271c1542d268adca6de75c435b63f8d9eb3
7EN <arnakorn567gmail.com>
2024-12-17 14:07:38 +0700

    first commit

--- controller/employee/employee.go
+++ controller/employee/employee.go
@@ -4,13 +4,25 @@ import (
 	"net/http"
 
 	"backend/api/db"
+
 	"github.com/gin-gonic/gin"
 )
 
 type Tbl_employee struct {
-	Emp_id        int
-	Emp_firstname string
-	Emp_lastname  string
+	Emp_id         int
+	Emp_firstname  string
+	Emp_lastname   string
+	Emp_salary     float64
+	Emp_department string
+}
+
+// Biding from Employee JSON
+type EmployeeBody struct {
+	Emp_id         int     `json:"emp_id" binding:"required"`
+	Emp_firstname  string  `json:"emp_firstname" binding:"required"`
+	Emp_lastname   string  `json:"emp_lastname" binding:"required"`
+	Emp_department string  `json:"emp_department" binding:"required"`
+	Emp_salary     float64 `json:"emp_salary" binding:"required"`
 }
 
 // GET
@@ -65,16 +77,30 @@ func DELETEEmployeeByID(c *gin.Context) {
 		"message": "get out" + id,
 	})
 }
+
+// get DB
 func GetEmployeeDB(c *gin.Context) {
 	var employees []Tbl_employee
 	db.Db.Find(&employees)
 	c.JSON(http.StatusOK, gin.H{"status": "ok", "message": "Employee Read Success", "employees": employees})
 }
 
+// POST Employee to Database
 func PostEmployeeDB(c *gin.Context) {
-	var employees []Tbl_employee
-	db.Db.Find(&employees)
-	c.JSON(http.StatusOK, gin.H{"status": "ok", "message": "Employee Read Success", "employees": employees})
+	var json EmployeeBody
+	if err := c.ShouldBindJSON(&json); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	tbl_employee := Tbl_employee{Emp_id: json.Emp_id, Emp_firstname: json.Emp_firstname, Emp_lastname: json.Emp_lastname, Emp_department: json.Emp_department, Emp_salary: json.Emp_salary}
+	db.Db.Create(&tbl_employee)
+	if tbl_employee.Emp_firstname != "" {
+		c.JSON(http.StatusOK, gin.H{"status": "ok", "message": "Employee Created", "tbl_fund": tbl_employee})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"status": "error", "message": "User Failed", "tbl_fund": tbl_employee})
+	}
+
 }
 
 func PUTEmployeeDB(c *gin.Context) {
---
+++ main_05.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	EmployeeController "backend/api/controller/employee"
+	"backend/api/db"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	//Get .env
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	//get InitDB fuction
+	db.InitDB()
+
+	router := gin.Default()
+	//Employee API Method
+	router.GET("/employees", EmployeeController.GET)
+	router.POST("/employees", EmployeeController.POST)
+	router.PUT("/employees", EmployeeController.PUT)
+	router.DELETE("/employees", EmployeeController.DELETE)
+
+	router.GET("/employees/:id", EmployeeController.GetEmployeeByID)       //GET By ID
+	router.POST("/employees/:id", EmployeeController.PostEmployeeByID)     //GET By ID
+	router.PUT("/employees/:id", EmployeeController.PUTEmployeeByID)       //GET By ID
+	router.DELETE("/employees/:id", EmployeeController.DELETEEmployeeByID) //GET By ID
+
+	router.GET("/employeesdb", EmployeeController.GetEmployeeDB)       //GET DB
+	router.POST("/employeesdb", EmployeeController.PostEmployeeDB)     //GET DB
+	router.PUT("/employeesdb", EmployeeController.PUTEmployeeDB)       //GET DB
+	router.DELETE("/employeesdb", EmployeeController.DELETEEmployeeDB) //GET DB
+
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
